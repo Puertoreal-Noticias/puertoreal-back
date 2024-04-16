ddcd3256a8fbd18844870800fda5fc8504d22190
Carlos jesus <carlosjesus.moralespica2000@gmail.com>
2024-04-16 16:24:55 +0200

    Creacion de bases de datos y enlazadas con los endpoints

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import accountRouter from "./routes/account.js";
 import authRouter from "./routes/auth.js";
 import authToken from "./routes/authToken.js";
 import authSession from "./routes/authSession.js";
-
+import mongoose from "mongoose";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
@@ -19,10 +19,12 @@ expressApp.use("/auth", authRouter);
 expressApp.use("/authToken", authToken);
 expressApp.use("/authSession", authSession);
 
-expressApp.listen(PORT, () => {
-  console.log(`Servidor levantado en el puerto, ${PORT}`);
-});
-
+const bootstrap = async () => {
+  await mongoose.connect(process.env.MONGODB_URL);
+  expressApp.listen(PORT, () => {
+    console.log(`Servidor levantado en el puerto, ${PORT}`);
+  });
+};
 // expressApp.post("/cuenta", (req, res) => {
 //   console.log(req.body, "BODY");
 //   console.log(req.query, "query");
@@ -30,3 +32,4 @@ expressApp.listen(PORT, () => {
 //   res.status(200).send(req.body);
 // console.log(req);
 // });
+bootstrap();
--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { USERS_BBDD } from "../bbdd.js";
+import userModel from "../schemas/user-schema.js";
 
 const accountRouter = express.Router();
 accountRouter.use((req, res, next) => {
@@ -8,11 +9,9 @@ accountRouter.use((req, res, next) => {
 });
 
 // Obtener los Detalles de una cuenta
-accountRouter.get("/:guid", (req, res) => {
+accountRouter.get("/:guid", async (req, res) => {
   const guid = req.params.guid;
-  const user = USERS_BBDD.find((user) => {
-    return user.guid === guid;
-  });
+  const user = await userModel.findById(guid).exec();
   if (!user) {
     return res.sendStatus(404);
   } else {
@@ -20,32 +19,34 @@ accountRouter.get("/:guid", (req, res) => {
   }
 });
 // Crear una nueva cuenta a partir de guid y name
-accountRouter.post("", (req, res) => {
+accountRouter.post("", async (req, res) => {
   const { guid, name } = req.body;
-  const user = USERS_BBDD.find((user) => {
-    return user.guid === guid;
-  });
+  const user = await userModel.findById(guid).exec();
+
+  // const user = USERS_BBDD.find((user) => {
+  //   return user.guid === guid;
+  // });
   if (!guid || !name) {
     return res.sendStatus(400);
   }
 
   if (user) {
-    return res.sendStatus(409);
+    return res.status(409).send("Usuario ya registrado");
   }
-
-  USERS_BBDD.push({
-    guid,
-    name,
-  });
-  return res.send();
+  const newUser = new userModel({ _id: guid, name });
+  await newUser.save();
+  // USERS_BBDD.push({
+  //   guid,
+  //   name,
+  // });
+  return res.send("Usuario registrado");
 });
 // Actualizar una cuenta
-accountRouter.patch("/:guid", (req, res) => {
+accountRouter.patch("/:guid", async (req, res) => {
   const guid = req.params.guid;
   const { name } = req.body;
-  const user = USERS_BBDD.find((user) => {
-    return user.guid === guid;
-  });
+  const user = await userModel.findById(guid).exec();
+
   if (!name) {
     return res.sendStatus(400);
   }
@@ -54,21 +55,47 @@ accountRouter.patch("/:guid", (req, res) => {
   }
   // user.
   user.name = name;
-  return res.send();
-});
-// Eliminar una cuenta
-accountRouter.delete("/:guid", (req, res) => {
-  const guid = req.params.guid;
-  const userIndex = USERS_BBDD.findIndex((user) => {
-    return user.guid === guid;
-  });
-  if (userIndex === -1) {
-    console.log("HA ENTRADO MAL?");
-    return res.sendStatus(404);
-  }
 
-  USERS_BBDD.splice(userIndex, 1);
-  return res.send();
+  await user.save();
+  return res.send("Usuario modificado");
 });
+
+// Eliminar una cuenta
+accountRouter.delete("/:guid", async (req, res) => {
+  const guid = req.params.guid;
+
+  try {
+    const result = await userModel.deleteOne({ _id: guid }).exec();
+
+    if (result.deletedCount === 0) {
+      console.log("No se encontró el usuario para eliminar");
+      return res.sendStatus(404);
+    }
+
+    return res.send("Usuario eliminado");
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500); // Devuelve un error 500 si algo sale mal
+  }
+});
+
+// Eliminar una cuenta
+// accountRouter.delete("/:guid", async (req, res) => {
+//   const guid = req.params.guid;
+//   const user = await userModel.findById(guid).exec();
+
+//   if (!user) {
+//     console.log("HA ENTRADO MAL?");
+//     return res.sendStatus(404);
+//   }
+
+//   try {
+//     await user.remove();
+//     return res.send("Usuario eliminado");
+//   } catch (error) {
+//     console.error(error);
+//     return res.sendStatus(500); // Devuelve un error 500 si algo sale mal
+//   }
+// });
 
 export default accountRouter;
